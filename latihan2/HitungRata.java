c355d1e936f34c2b4875bcf08bac5de5187d3649
laysernx4 <kero.deai@gmail.com>
2019-02-07 23:36:48 +0700

    menghitung nilai rata-rata

---
+++ latihan2/HitungRata.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class HitungRata {
+    public static void main (String[] args){
+        double rataRata;
+        String emot;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Berapa nilai ujian ke-1 : ");
+        int i1 = input.nextInt();
+
+        System.out.print("Berapa nilai ujian ke-2 : ");
+        int i2 = input.nextInt();
+
+        System.out.print("Berapa nilai ujian ke-3 : ");
+        int i3 = input.nextInt();
+
+        rataRata = (i1 + i2 + i3) / 3;
+
+        if (rataRata >= 70){
+            emot = ":-)";
+        }
+        else{
+            emot = ":-(";
+        }
+
+        System.out.println("Nilai Rata-Rata Kamu adalah : " + rataRata);
+        System.out.println(emot);
+    }
+}
