bd7007a3e5c9faa256bc13f29afa666e2f148dc8
laysernx4 <kero.deai@gmail.com>
2019-02-08 00:26:21 +0700

    pake if sama switch

--- latihan2/HitungNilai.java
+++ latihan2/HitungNilai.java
@@ -4,9 +4,12 @@ public class HitungNilai {
     public static void main(String[] args) {
         Scanner i = new Scanner(System.in);
         String nilai;
+        String nilai2;
+        String grade;
         System.out.print("Masukkan Nilai Anda : A/B/C/D/E");
         System.out.print("\nNilai Anda = ");
         nilai = i.nextLine();
+        //pakeSwitch
         switch (nilai){
             case "A" :
                 System.out.print("\nExcellent");
@@ -28,6 +31,34 @@ public class HitungNilai {
                 break;
             default:
                 System.out.print("\nSorry, you failed");
+
+
+            //pakeIF
+                System.out.print("Masukkan Nilai (A,B,C,D,E) : ");
+
+                nilai2 = i.nextLine();
+
+                if (nilai2.equals('A')) {
+                    grade = "Excellent";
+                }
+                else if (nilai2.equals('B')) {
+                    grade = "Good job!";
+                }
+                else if (nilai2.equals('C')) {
+                    grade = "Study harder!";
+                }
+                else if (nilai2.equals('A')) {
+                    grade = "Excellent";
+                }
+                else if (nilai2.equals('B')) {
+                    grade = "Good job!";
+                }
+                else if (nilai2.equals('C')) {
+                    grade = "Study harder!";
+                }
+                else {
+                    grade = "Sorry, you failed.";
+                }
         }
     }
 }
