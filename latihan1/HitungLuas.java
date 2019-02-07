f0f53145e8a466e7d28bdd116098885636282ae1
laysernx4 <kero.deai@gmail.com>
2019-02-07 22:51:52 +0700

    belajar dari merangkak lagi

---
+++ latihan1/HitungLuas.java
@@ -0,0 +1,31 @@
+public class HitungLuas {
+
+    public static void main(String[] args){
+
+    }
+
+}
+
+
+
+public class cariLuasPersegiPanjang {
+
+    public static void main (String[] args){
+
+        int panjang , lebar ;
+
+    }
+
+}
+
+
+
+public class cariLuasLingkaran {
+
+    public static void main (String[] args){
+
+        int jari-jari ;
+
+    }
+
+}
