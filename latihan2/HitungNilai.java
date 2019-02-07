4d8016de209f1a9c249ec77ebd29a965cae2e25c
laysernx4 <kero.deai@gmail.com>
2019-02-08 00:46:46 +0700

    pake IF

--- latihan2/HitungNilai.java
+++ latihan2/HitungNilai.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class HitungNilai {
     public static void main(String[] args) {
         Scanner i = new Scanner(System.in);
-        String nilai;
-        String nilai2;
+        //String nilai;
+        char nilai2;
         String grade;
-        System.out.print("Masukkan Nilai Anda : A/B/C/D/E");
+/*        System.out.print("Masukkan Nilai Anda : A/B/C/D/E");
         System.out.print("\nNilai Anda = ");
         nilai = i.nextLine();
         //pakeSwitch
@@ -32,33 +32,35 @@ public class HitungNilai {
             default:
                 System.out.print("\nSorry, you failed");
 
-
+*/
             //pakeIF
+
                 System.out.print("Masukkan Nilai (A,B,C,D,E) : ");
+                nilai2 = i.next().charAt(0);
 
-                nilai2 = i.nextLine();
-
-                if (nilai2.equals('A')) {
+                if (nilai2 == 'A') {
                     grade = "Excellent";
                 }
-                else if (nilai2.equals('B')) {
+                else if (nilai2 == 'B') {
                     grade = "Good job!";
                 }
-                else if (nilai2.equals('C')) {
+                else if (nilai2 == 'C') {
                     grade = "Study harder!";
                 }
-                else if (nilai2.equals('A')) {
+                else if (nilai2 == 'a') {
                     grade = "Excellent";
                 }
-                else if (nilai2.equals('B')) {
+                else if (nilai2 == 'b') {
                     grade = "Good job!";
                 }
-                else if (nilai2.equals('C')) {
+                else if (nilai2 == 'c') {
                     grade = "Study harder!";
                 }
                 else {
                     grade = "Sorry, you failed.";
                 }
+
+                System.out.println(grade);
         }
     }
-}
+//}
